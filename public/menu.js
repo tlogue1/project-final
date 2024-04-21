e46c33ff50d834b462c37655b6ea4cfa2676cdd6
tyler logue <tylerlogue3@gmail.com>
2024-04-21 16:53:41 -0400

    updates

--- public/menu.js
+++ public/menu.js
@@ -1,64 +1,254 @@
+
+const getBagels= async () => {
+    try {
+        return (await fetch("api/bagels/")).json();
+    } catch(error) {
+        console.log(error);
+    }
+};
+
+const showBagels = async () => {
+    let bagels = await getBagels();
+    let bagelsDiv = document.getElementById("bagels-list");
+    bagelsDiv.innerHTML = "";
+    bagels.forEach((bagel) => {
+      const bagelDiv = document.createElement("div");
+      bagelDiv.classList.add("bagel");
+      bagelsDiv.append(bagelDiv);
+  
+      const nameDiv = document.createElement("div");
+      nameDiv.classList.add("bagel-name");
+      nameDiv.textContent = bagel.name;
+      bagelDiv.append(nameDiv);
+  
+      const imgDiv = document.createElement("div");
+      imgDiv.classList.add("bagel-img");
+  
+      const img = document.createElement("img");
+      img.src = bagel.img;
+      imgDiv.append(img);
+      bagelDiv.append(imgDiv);
+  
+      img.onclick = (e) => {
+        e.preventDefault();
+        displayDetails(bagel);
+      };
+    });
+  };
+/*
+
+const showBagels = async () => {
+    let bagels = await getBagels();
+    let bagelsDiv = document.getElementById("bagels-list");
+    bagelsDiv.innerHTML = "";
+    bagels.forEach((bagel) => {
+        const section = document.createElement("section");
+        section.classList.add("bagel");
+        bagelsDiv.append(section);
+
+        const a = document.createElement("a");
+        a.href = "#";
+        section.append(a);
+        
+    
+        const img = document.createElement("img");
+        img.src = bagel.img;
+        a.append(img);
+    
+        a.onclick = (e) => {
+          e.preventDefault();
+          displayDetails(bagel);
+        };
+      });
+    };
+*/
+
+const displayDetails = (bagel) => {
+    openDialog("bagel-details");
+    const bagelDetails = document.getElementById("bagel-details");
+    bagelDetails.innerHTML = "";
+    bagelDetails.classList.remove("hidden");
+
+    const bagelInner = document.createElement("div");
+    bagelInner.classList.add("bagel-inner");
+
+    const img = document.createElement("img");
+    img.src = bagel.img;
+    bagelInner.appendChild(img);
+
+    const details = document.createElement("div");
+    details.classList.add("details");
+    
+    const h3 = document.createElement("h3");
+    h3.innerHTML = bagel.name;
+    details.appendChild(h3);
+
+    const p = document.createElement("p");
+    p.innerHTML = bagel.description;
+    details.appendChild(p);
+
+    const ul = document.createElement("ul");
+    bagel.ingredients.forEach((ingredient) => {
+        const li = document.createElement("li");
+        li.innerHTML = ingredient;
+        ul.appendChild(li);
+    });
+    details.appendChild(ul);
+
+    const deletebutton = document.createElement("a");
+        deletebutton.innerHTML = "	&#9249;";
+        details.appendChild(deletebutton);
+        deletebutton.id = "delete-button";
+      
+        const editButton = document.createElement("a");
+        editButton.innerHTML = "&#9998; ";
+        details.appendChild(editButton);
+        editButton.id = "edit-button";
+
+        bagelInner.appendChild(img);
+        bagelInner.appendChild(details);
+        bagelDetails.append(bagelInner);
+
+        editButton.onclick = showBagelForm;
+        deletebutton.onclick = deleteItem.bind(this, bagel);
+        
+    fillEditForm(bagel);
+};
+
+const fillEditForm = (bagel) => {
+    const form = document.getElementById("bagel-form");
+    form._id.value = bagel._id;
+    form.name.value = bagel.name;
+    form.description.value = bagel.description;
+    document.getElementById("img-prev").src = bagel.img;
+
+    fillIngredients(bagel.ingredients);
+};
+
+const fillIngredients = (ingredients) => {
+    const section = document.getElementById("ingredients-boxes");
+    ingredients.forEach((ingredient) => {
+        const input = document.createElement("input");
+        input.type = "text";
+        input.value = ingredient;
+        section.append(input);
+    });
+};
+
+const addEditBagel = async (e) => {
+    e.preventDefault();
+    const form = document.getElementById("bagel-form");
+    const formData = new FormData(form);
+    let response;
+    formData.append("ingredients", getIngredients());
+
+    console.log(...formData);
+
+    if(form._id.value.trim() == "") {
+        console.log("in post");
+        response = await fetch("/api/bagels", {
+            method: "POST",
+            body: formData,
+        });
+    } else {
+        console.log("in put");
+        response = await fetch(`/api/bagels/${form._id.value}`, {
+            method:"PUT",
+            body:formData,
+        });
+    }
+
+    if(response.status != 200) {
+        console.log("Error adding or editing bagel");
+    }
+
+    await response.json();
+    resetForm();
+    document.getElementById("dialog").style.display = "none";
+    showBagels();
+};
+
+const getIngredients = () => {
+    const inputs = document.querySelectorAll("#ingredients-boxes input");
+    let ingredients = [];
+
+    inputs.forEach((input) => {
+        ingredients.push(input.value);
+    });
+    return ingredients;
+};
+
+const resetForm = () => {
+    const form = document.getElementById("bagel-form");
+    form.reset();
+    form._id.value = "";
+    document.getElementById("ingredients-boxes").innerHTML = "";
+    document.getElementById("img-prev").src = "";
+};
+
+const showBagelForm = (e) => {
+    openDialog("bagel-form");
+    console.log(e.target);
+    if(e.target.getAttribute("id") != "edit-button") {
+        resetForm();
+    }
+};
+
+const deleteItem = async(bagel) => {
+    let response = await fetch(`/api/bagels/${bagel._id}`, {
+        method:"DELETE",
+        headers:{
+            "Content-Type":"application/json;charset=utf-8",
+    },
+  });
+
+    if(response.status != 200) {
+        console.log("error deleting");
+        return;
+    }
+
+    let result = await response.json();
+    resetForm();
+    showBagels();
+    document.getElementById("dialog").style.display = "none";
+};
+
+const addIngredient = (e) => {
+    e.preventDefault();
+    const section = document.getElementById("ingredients-boxes");
+    const input = document.createElement("input");
+    input.type = "text";
+    section.append(input);
+};
+
+const openDialog = (id) => {
+    document.getElementById("dialog").style.display = "block";
+    document.querySelectorAll("#dialog-details > *").forEach((item) => {
+        item.classList.add("hidden");
+    });
+    document.getElementById(id).classList.remove("hidden");
+};
+
+showBagels();
+document.getElementById("bagel-form").onsubmit = addEditBagel;
+document.getElementById("add-button").onclick = showBagelForm;
+document.getElementById("add-ingredients").onclick = addIngredient;
+
+
+document.getElementById("img").onchange = (e) => {
+    if(!e.target.files.length) {
+        document.getElementById("img-prev").src = "";
+        return;
+    }
+    document.getElementById("img-prev").src = URL.createObjectURL(e.target.files.item(0));
+};
+
+
 const toggleHamburger = () => {
     document.getElementById("nav-items").classList.toggle("hide");
 };
 
 window.onload = () => {
     document.getElementById("hamburger").onclick = toggleHamburger;
+    showBagels();
 };
-
-/*popup from w3 school
-function myFunction() {
-    var popup = document.getElementById("myPopup");
-    popup.classList.toggle("show");
-}
-*/
-
-
-const getMenuItems = async () => {
-    const url = "menu.json";
-    try {
-        const response = await fetch(url);
-        return response.json();
-    } catch (error) {
-        console.log(error);
-    }
-};
-
-const showMenuItems = async () => {
-    const items = await getMenuItems();
-    const allSections = document.getElementById("menu-items");
-  
-    items.forEach((item) => {
-      const itemSection = document.createElement("section");
-      allSections.append(itemSection);
-  
-      itemSection.classList.add("item");
-  
-      const h3 = document.createElement("h3");
-      h3.innerHTML = item.name;
-      itemSection.append(h3);
-  
-      const img = document.createElement("img");
-      img.src = `images/${item.img_name}`;
-      img.alt = item.name;
-      itemSection.append(img);
-  
-      const toasted = document.createElement("p");
-      toasted.innerHTML = `Toasted: ${item.toasted}`;
-      itemSection.append(toasted);
-  
-      const catering = document.createElement("p");
-      catering.innerHTML = `Catering: ${item.catering}`;
-      itemSection.append(catering);
-  
-      const ingredients = document.createElement("ul");
-      item.ingredients.forEach((ingredient) => {
-        const li = document.createElement("li");
-        li.textContent = ingredient;
-        ingredients.append(li);
-      });
-      itemSection.append(ingredients);
-    });
-  };
-  
-  showMenuItems();
--- server.js
+++ server.js
@@ -1,15 +1,18 @@
 const express = require("express");
 const app = express();
 const Joi = require("joi");
-const multer = resquire("multer");
+const multer = require("multer");
 app.use(express.static("public"));
 app.use("/uploads", express.static("uploads"));
 app.use(express.json());
 const cors = require("cors");
 app.use(cors());
 const mongoose = require("mongoose");
-const upload = multer({dest: __dirnmae + "/public/images"});
+const upload = multer({dest: __dirname + "/public/images/menu"});
 
+
+
+/*
 const storage = multer.diskStorage({
     destination: (req, file, cb) => {
         cb(null, "./public/images/");
@@ -18,10 +21,131 @@ const storage = multer.diskStorage({
         cb(null, file.originalname);
     }
 });
-
+*/
 mongoose
-    .connect("")//add my mongoose thing here at the end
+    .connect("mongodb+srv://tylerlogue3:HwKBox8tlEn2Ylb8@work.5tc5e9m.mongodb.net/?retryWrites=true&w=majority")
     .then(() => console.log("connected to MongoDB"))
-    .catch(error => console.log("couldmt connect to mongodb", error));
+    .catch(error => console.log("couldn't connect to mongodb", error));
 
-    
+    const bagelSchema = new mongoose.Schema({
+        name: String,
+        description: String,
+        ingredients: [String],
+        img: String,
+    });
+
+    const Bagel = mongoose.model("bagel", bagelSchema);
+
+    app.get("/", (req, res) => {
+        res.sendFile(__dirname + "/mainPage.html");
+    });
+
+    app.get("/menu", (req, res) => {
+        res.sendFile(__dirname + "/menu.html");
+    });
+
+    app.get("/catering", (req, res) => {
+        res.sendFile(__dirname + "/catering.html");
+    });
+
+    app.get("/contacts", (req, res) => {
+        res.sendFile(__dirname + "/contacts.html");
+    });
+
+    app.get("/locations", (req, res) => {
+        res.sendFile(__dirname + "/locations.html");
+    });
+
+
+
+
+    app.post("/api/bagels", upload.single("img"), (req, res) => {
+        const result = validateBagel(req.body);
+        if(result.error) {
+            res.status(400).send(result.error.details[0].message);
+            return;
+        }
+
+        const bagel = new Bagel({
+            name: req.body.name,
+            description: req.body.description,
+            ingredients: req.body.ingredients.split(","),
+        });
+
+        if(req.file) {
+            bagel.img = "public/images/" + req.file.filename;
+        }
+        createBagel(bagel, res);
+    });
+
+    app.get("/api/bagels", (req, res) => {
+        getBagels(res);
+    });
+
+    const getBagels = async (res) => {
+        const bagels = await Bagel.find();
+        res.send(bagels);
+    };
+
+    app.get("/api/bagels/:id", (req, res) => {
+        getBagel(req.params.id, res);
+    });
+
+    const getBagel = async (id, res) => {
+        const bagel = await Bagel.findOne({_id: id });
+        res.send(bagel);
+    };
+
+    const createBagel = async (bagel, res) => {
+        const result = await bagel.save();
+        res.send(result);
+    };
+
+    app.delete("/api/bagels/:id", (req, res) => {
+        deleteBagel(res, req.params.id);
+    });
+
+    const deleteBagel = async (res, id) => {
+        const bagel = await Bagel.findByIdAndDelete(id);
+        res.send(bagel);
+    };
+
+    app.put("/api/bagels/:id", upload.single("img"), (req, res) => {
+        const result = validateBagel(req.body);
+        if(result.error) {
+            res.status(400).send(result.error.details[0].message);
+            return;
+        }
+        updateBagel(req, res);
+    });
+
+    const updateBagel = async (req, res) => {
+        let field = {
+            name: req.body.name,
+            description: req.body.description,
+            ingredients: req.body.ingredients.split(","),
+        }
+        if(req.file) {
+            field.img = "images/menu/" + req.file.filename;
+        }
+        const result = await Bagel.updateOne({_id: req.params.id }, field);
+
+        res.send(result);
+    };
+
+    const validateBagel = (bagel) => {
+        const schema = Joi.object({
+            name: Joi.string().min(3).required(),
+            description: Joi.string().min(3).required(),
+            ingredients: Joi.allow("").required(),
+            _id: Joi.allow(""),
+            img: Joi.object({
+                filename: Joi.string().required(),
+            }),
+        });
+    return schema.validate(bagel);
+};
+
+app.listen(3008, () => {
+    console.log("I'm Listening");
+});
