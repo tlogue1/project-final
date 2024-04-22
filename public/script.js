e8c48d32e25bfcfd88fdfde21f653a8638de5f61
tyler logue <tylerlogue3@gmail.com>
2024-04-21 23:04:04 -0400

    menu

--- public/script.js
+++ public/script.js
@@ -11,3 +11,26 @@ window.onload = () => {
 };
 
 
+var modal = document.getElementById("myModal");
+var button = document.getElementById("location-button");
+var span = document.getElementsByClassName("close")[0];
+button.onclick = function() {
+    modal.style.display = "block";
+  }
+  span.onclick = function() {
+    modal.style.display = "none";
+  }
+  window.onclick = function(event) {
+    if (event.target == modal) {
+      modal.style.display = "none";
+    }
+  }
+  const submitButton = document.querySelector("#submit-button");
+
+
+submitButton.addEventListener("click", () => {
+  modal.style.display = "none";
+  console.log("submitted");
+});
+
+
